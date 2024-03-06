58ca95dcafeb1af58f922eb09c53a8aeef777257
Faraneh Javadinejad <faraneh.javadinejad@points.com>
2024-03-05 19:10:40 -0500

    backToNormal

--- grocalife/src/App.tsx
+++ grocalife/src/App.tsx
@@ -1,15 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TaskSummaryPage from "./components/pages/summaryPages/taskSummaryPage";
-import { TaskCard } from "./components/utils/_Classes/taskClasses";
 import NewTaskCard from "./components/utils/taskCard/newTaskCard";
 
 import './App.css'
 
 
 function App() {
-
-  const task1 = new TaskCard({id: 1001, creator: 'user1', createdate: [2024, 1, 2]})
-  task1.getCardData()
   
   return (
     <>
